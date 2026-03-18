4fdad4a638f41c182963b7b640979f8b2b5ff01c
Oldes Huhuman <oldes.huhuman@gmail.com>
2026-03-18 15:37:52 +0100

    CHANGE: HSV related code optimization

--- src/core/n-image.c
+++ src/core/n-image.c
@@ -86,47 +86,53 @@ typedef struct REBCLR {
 	}
 #ifdef HSV_CONVERSION_USING_DECIMAL
 	
-	REBDEC h = (float)val->h;
-	REBDEC s = (float)val->s / 255.0;
-	REBDEC v = (float)val->v / 255.0;
+	REBDEC h = val->h / (255.0 / 6);  // NOTE: 255 because we have just one byte! Else it should be 360!
+	REBDEC s = val->s * (1.0 / 255.0);
+	REBDEC v = val->v * (1.0 / 255.0);
 
-	int i;
-	REBDEC f, p, q, t;
+	int i = (int)h;
+	REBDEC f = h - i;
+	REBDEC p = v * (1.0 - s);
+	REBDEC q = v * (1.0 - s * f);
+	REBDEC t = v * (1.0 - s * (1.0 - f));
 
-	h /= 42.5;    // sector 0 to 5; we are using 255 instead of 360 because the value is in tuple's byte ! (255 / 6)
-	i = floor(h);
-	f = h - i;    // factorial part of h
-	p = v * (1 - s);
-	q = v * (1 - s * f);
-	t = v * (1 - s * (1 - f));
+	// Pre-multiply v once instead of repeating 255.0 * v in every case
+	REBDEC v255 = 255.0 * v;
+	REBDEC p255 = 255.0 * p;
+	REBDEC q255 = 255.0 * q;
+	REBDEC t255 = 255.0 * t;
 	switch (i) {
-		case 0:  val->r = (REBYTE)(255.0 * v); val->g = (REBYTE)(255.0 * t); val->b = (REBYTE)(255.0 * p); break;
-		case 1:  val->r = (REBYTE)(255.0 * q); val->g = (REBYTE)(255.0 * v); val->b = (REBYTE)(255.0 * p); break;
-		case 2:  val->r = (REBYTE)(255.0 * p); val->g = (REBYTE)(255.0 * v); val->b = (REBYTE)(255.0 * t); break;
-		case 3:  val->r = (REBYTE)(255.0 * p); val->g = (REBYTE)(255.0 * q); val->b = (REBYTE)(255.0 * v); break;
-		case 4:  val->r = (REBYTE)(255.0 * t); val->g = (REBYTE)(255.0 * p); val->b = (REBYTE)(255.0 * v); break;
-		default: val->r = (REBYTE)(255.0 * v); val->g = (REBYTE)(255.0 * p); val->b = (REBYTE)(255.0 * q); break; // case 5
+	case 0:  val->r = (REBYTE)v255; val->g = (REBYTE)t255; val->b = (REBYTE)p255; break;
+	case 1:  val->r = (REBYTE)q255; val->g = (REBYTE)v255; val->b = (REBYTE)p255; break;
+	case 2:  val->r = (REBYTE)p255; val->g = (REBYTE)v255; val->b = (REBYTE)t255; break;
+	case 3:  val->r = (REBYTE)p255; val->g = (REBYTE)q255; val->b = (REBYTE)v255; break;
+	case 4:  val->r = (REBYTE)t255; val->g = (REBYTE)p255; val->b = (REBYTE)v255; break;
+	default: val->r = (REBYTE)v255; val->g = (REBYTE)p255; val->b = (REBYTE)q255; break;
 	}
 #else
-	REBYTE region, remainder, p, q, t;
-	
-	region = val->h / 43; // six regions: 255 / 6
-	remainder = (val->h - (region * 43)) * 6;
+	REBYTE region = val->h / 43;
+	REBYTE remainder = (val->h - region * 43) * 6;
 
-	p = (val->v * (255 - val->s)) >> 8;
-	q = (val->v * (255 - ((val->s * remainder) >> 8))) >> 8;
-	t = (val->v * (255 - ((val->s * (255 - remainder)) >> 8))) >> 8;
+	// Use REBINT (16-bit+) intermediates to avoid implicit promotion warnings
+	REBYTE p = (val->v * (255 - val->s)) >> 8;
+	REBYTE q = (val->v * (255 - ((val->s * remainder) >> 8))) >> 8;
+	REBYTE t = (val->v * (255 - ((val->s * (255 - remainder)) >> 8))) >> 8;
 
-	switch (region)	{
-		case 0:	val->r = val->v; val->g = t; val->b = p; break;
-		case 1:	val->r = q; val->g = val->v; val->b = p; break;
-		case 2:	val->r = p; val->g = val->v; val->b = t; break;
-		case 3:	val->r = p; val->g = q; val->b = val->v; break;
-		case 4:	val->r = t; val->g = p; val->b = val->v; break;
-		default: // case 5
-			val->r = val->v; val->g = p; val->b = q;
-			break;
-	}
+	// Lookup table to avoid branching entirely
+	static const REBYTE idx[6][3] = {
+		// r     g     b      (0=v, 1=t, 2=p, 3=q)
+		{  0,    1,    2  },  // case 0
+		{  3,    0,    2  },  // case 1
+		{  2,    0,    1  },  // case 2
+		{  2,    3,    0  },  // case 3
+		{  1,    2,    0  },  // case 4
+		{  0,    2,    3  },  // case 5
+	};
+	const REBYTE components[4] = { val->v, t, p, q };
+	const REBYTE* row = idx[region];
+	val->r = components[row[0]];
+	val->g = components[row[1]];
+	val->b = components[row[2]];
 #endif
     return R_ARG1;
 }
@@ -173,11 +179,11 @@ typedef struct REBCLR {
 	val->s = (REBYTE)(255 * delta / rgbMax);
 
 	if (rgbMax == r)
-		val->h = 0 + 43 * (g - b) / delta;
+		val->h = (REBYTE)(0 + 43 * (int)(g - b) / delta);
 	else if (rgbMax == g)
-		val->h = 85 + 43 * (b - r) / delta;
+		val->h = (REBYTE)(85 + 43 * (int)(b - r) / delta);
 	else
-		val->h = 171 + 43 * (r - g) / delta;
+		val->h = (REBYTE)(170 + 43 * (int)(r - g) / delta);
 #endif
 
 	return R_ARG1;
